09982f107c99b67978ba598cc3d718ac5ec8c4c0
Magnus Bakke <magnusbakke@live.com>
2017-03-07 21:19:39 +0100

    Various
    - Removed the option of running a method with any signature
    (DynamicInvoke) from the ThreadStarter class, as apparently, it was
    favored when early binding methods would suffice.
    
    - Made various classes for the setup program, and wrote most of the
    setup program.

---
+++ Gruppe21-Prosjekt-Alpha/LoggInn_Admin.Designer.vb
@@ -0,0 +1,111 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class LoggInn_Admin
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PicLoadingSurface = New System.Windows.Forms.PictureBox()
+        Me.GroupLoggInn = New System.Windows.Forms.GroupBox()
+        Me.txtBrukernavn = New System.Windows.Forms.TextBox()
+        Me.txtPassord = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        CType(Me.PicLoadingSurface, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.GroupLoggInn.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'PicLoadingSurface
+        '
+        Me.PicLoadingSurface.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.PicLoadingSurface.Location = New System.Drawing.Point(133, 207)
+        Me.PicLoadingSurface.Name = "PicLoadingSurface"
+        Me.PicLoadingSurface.Size = New System.Drawing.Size(32, 32)
+        Me.PicLoadingSurface.TabIndex = 9
+        Me.PicLoadingSurface.TabStop = False
+        '
+        'GroupLoggInn
+        '
+        Me.GroupLoggInn.Controls.Add(Me.txtBrukernavn)
+        Me.GroupLoggInn.Controls.Add(Me.txtPassord)
+        Me.GroupLoggInn.Controls.Add(Me.Label1)
+        Me.GroupLoggInn.Controls.Add(Me.Label2)
+        Me.GroupLoggInn.Location = New System.Drawing.Point(13, 71)
+        Me.GroupLoggInn.Name = "GroupLoggInn"
+        Me.GroupLoggInn.Size = New System.Drawing.Size(258, 119)
+        Me.GroupLoggInn.TabIndex = 10
+        Me.GroupLoggInn.TabStop = False
+        Me.GroupLoggInn.Text = "Logg inn"
+        '
+        'txtBrukernavn
+        '
+        Me.txtBrukernavn.Location = New System.Drawing.Point(73, 32)
+        Me.txtBrukernavn.Name = "txtBrukernavn"
+        Me.txtBrukernavn.Size = New System.Drawing.Size(173, 20)
+        Me.txtBrukernavn.TabIndex = 0
+        '
+        'txtPassord
+        '
+        Me.txtPassord.Location = New System.Drawing.Point(73, 58)
+        Me.txtPassord.Name = "txtPassord"
+        Me.txtPassord.Size = New System.Drawing.Size(173, 20)
+        Me.txtPassord.TabIndex = 1
+        Me.txtPassord.UseSystemPasswordChar = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(6, 35)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(62, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Brukernavn"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(22, 61)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(45, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Passord"
+        '
+        'LoggInn_Admin
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.GroupLoggInn)
+        Me.Controls.Add(Me.PicLoadingSurface)
+        Me.Name = "LoggInn_Admin"
+        Me.Text = "Form1"
+        CType(Me.PicLoadingSurface, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.GroupLoggInn.ResumeLayout(False)
+        Me.GroupLoggInn.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents PicLoadingSurface As PictureBox
+    Friend WithEvents GroupLoggInn As GroupBox
+    Friend WithEvents txtBrukernavn As TextBox
+    Friend WithEvents txtPassord As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+End Class
---
+++ Gruppe21-Prosjekt-Alpha/LoggInn_Admin.vb
@@ -0,0 +1,184 @@
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+
+Imports System.IO
+Imports System.Security.Cryptography
+Imports AudiopoLib
+
+Public Class LoggInn_Admin
+    Dim LoginHelper As MySqlAdminLogin
+    Dim LoadingGraphics As LoadingGraphics(Of PictureBox)
+    Dim WithEvents LayoutTool As FormLayoutTools
+    Dim WithEvents FWButton As FullWidthControl
+    Dim NotifManager As NotificationManager
+    Private Sub WhenFinished(ByVal Valid As Boolean)
+        MsgBox(Valid.ToString)
+    End Sub
+    Private Sub LoggInn_Admin_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        LoadingGraphics = New LoadingGraphics(Of PictureBox)(PicLoadingSurface)
+        LayoutTool = New FormLayoutTools(Me)
+        LoginHelper = New MySqlAdminLogin("mysql.stud.iie.ntnu.no", "magnbakk")
+        NotifManager = New NotificationManager(Me)
+        NotifManager.AssignedLayoutManager = LayoutTool
+        With LoadingGraphics
+            .Stroke = 2
+            .Pen.Color = Color.LimeGreen
+        End With
+        With LoginHelper
+            .WhenFinished = AddressOf WhenFinished
+        End With
+        With LayoutTool
+            .IncludeFormTitle = True
+            .CenterOnForm(GroupLoggInn)
+            .CenterSurface(PicLoadingSurface, GroupLoggInn)
+        End With
+        FWButton = New FullWidthControl(GroupLoggInn, True, FullWidthControl.SnapType.Bottom)
+        Dim GroupHeader As New FullWidthControl(GroupLoggInn, False, FullWidthControl.SnapType.Top)
+        With GroupHeader
+            .Height = 20
+            .BackColor = Color.FromArgb(230, 230, 230)
+            .ForeColor = Color.FromArgb(100, 100, 100)
+            .TextAlign = ContentAlignment.MiddleLeft
+            .Padding = New Padding(5, 0, 0, 0)
+            .Text = "Logg inn"
+        End With
+        Dim Test1 As New CredentialsManager
+        Test1.TestEncoding("Hei lol", "kek")
+        Test1.Decode("kek")
+    End Sub
+    Private Sub FWButton_Click(sender As Object, e As EventArgs) Handles FWButton.Click
+        For Each C As Control In GroupLoggInn.Controls
+            If Not C.GetType = GetType(FullWidthControl) Then
+                C.Hide()
+            End If
+        Next
+        FWButton.Hide()
+        FWButton.Enabled = False
+        LoginHelper.LoginAsync(txtBrukernavn.Text, txtPassord.Text)
+        LoadingGraphics.Spin(30, 10)
+        LayoutTool.Refresh()
+        txtBrukernavn.Clear()
+        txtPassord.Clear()
+        txtBrukernavn.Focus()
+    End Sub
+End Class
+
+
+
+Public NotInheritable Class EncryptedReadWrite
+    Implements IDisposable
+
+    Private TripleDes As New TripleDESCryptoServiceProvider
+    Private Function TruncateHash(ByVal key As String, ByVal length As Integer) As Byte()
+        Dim sha1 As New SHA1CryptoServiceProvider
+        Dim keyBytes() As Byte = System.Text.Encoding.Unicode.GetBytes(key)
+        'Dim hashList As New List(Of Byte)(sha1.ComputeHash(keyBytes))
+        Dim hash() As Byte = sha1.ComputeHash(keyBytes)
+
+        ' Truncate or pad the hash. ?????
+        ReDim Preserve hash(length - 1)
+        Return hash
+    End Function
+    Sub New(ByVal key As String)
+        TripleDes.Key = TruncateHash(key, TripleDes.KeySize \ 8)
+        TripleDes.IV = TruncateHash("", TripleDes.BlockSize \ 8)
+    End Sub
+    Public Function EncryptData(ByVal plaintext As String) As String
+        ' Convert the plaintext string to a byte array.
+        Dim plaintextBytes() As Byte = Text.Encoding.Unicode.GetBytes(plaintext)
+
+        ' Create the stream.
+        Dim ms As New IO.MemoryStream
+        ' Create the encoder to write to the stream.
+        Dim encStream As New CryptoStream(ms, TripleDes.CreateEncryptor(), CryptoStreamMode.Write)
+
+        ' Use the crypto stream to write the byte array to the stream.
+        encStream.Write(plaintextBytes, 0, plaintextBytes.Length)
+        encStream.FlushFinalBlock()
+
+        ' Convert the encrypted stream to a printable string.
+        Return Convert.ToBase64String(ms.ToArray)
+    End Function
+    Public Function DecryptData(ByVal encryptedtext As String) As String
+        Try
+            ' Convert the encrypted text string to a byte array.
+            Dim encryptedBytes() As Byte = Convert.FromBase64String(encryptedtext)
+
+            ' Create the stream.
+            Dim ms As New IO.MemoryStream
+            ' Create the decoder to write to the stream.
+            Dim decStream As New CryptoStream(ms, TripleDes.CreateDecryptor(), CryptoStreamMode.Write)
+
+            ' Use the crypto stream to write the byte array to the stream.
+            decStream.Write(encryptedBytes, 0, encryptedBytes.Length)
+            decStream.FlushFinalBlock()
+
+            ' Convert the plaintext stream to a string.
+            Return Text.Encoding.Unicode.GetString(ms.ToArray)
+        Catch
+            MsgBox("HEI LOL TEST")
+            Return "Feil"
+        End Try
+    End Function
+
+#Region "IDisposable Support"
+    Private disposedValue As Boolean ' To detect redundant calls
+
+    ' IDisposable
+    Protected Sub Dispose(disposing As Boolean)
+        If Not disposedValue Then
+            If disposing Then
+                TripleDes.Dispose()
+            End If
+
+            ' TODO: free unmanaged resources (unmanaged objects) and override Finalize() below.
+            ' TODO: set large fields to null.
+        End If
+        disposedValue = True
+    End Sub
+    Public Sub Dispose() Implements IDisposable.Dispose
+        ' Do not change this code.  Put cleanup code in Dispose(disposing As Boolean) above.
+        Dispose(True)
+        ' TODO: uncomment the following line if Finalize() is overridden above.
+        ' GC.SuppressFinalize(Me)
+    End Sub
+#End Region
+End Class
+
+Public Class CredentialsManager
+    Private DefPath As String
+    Public Sub New(Optional DefaultPath As String = "Default")
+        If Not DefaultPath = "Default" Then
+            DefPath = DefaultPath
+        Else
+            DefPath = Application.StartupPath & "\test\"
+        End If
+
+        If (Not System.IO.Directory.Exists(DefPath)) Then
+            System.IO.Directory.CreateDirectory(DefPath)
+        End If
+        If Not File.Exists(DefPath & "\test.txt") Then
+            File.Create(DefPath & "\test.txt")
+        End If
+    End Sub
+    Public Sub TestEncoding(ByVal Value As String, ByVal Password As String)
+        Dim wrapper As New EncryptedReadWrite(Password)
+        Dim cipherText As String = wrapper.EncryptData(Value)
+
+        MsgBox("The cipher text is: " & cipherText)
+        My.Computer.FileSystem.WriteAllText(DefPath & "\test.txt", cipherText, False)
+    End Sub
+    Public Sub Decode(ByVal Password As String)
+        Dim cipherText As String = My.Computer.FileSystem.ReadAllText(DefPath & "\test.txt")
+        Dim wrapper As New EncryptedReadWrite(Password)
+
+        ' DecryptData throws if the wrong password is used.
+        Try
+            Dim plainText As String = wrapper.DecryptData(cipherText)
+            MsgBox("The plain text is: " & plainText)
+        Catch ex As System.Security.Cryptography.CryptographicException
+            MsgBox("The data could not be decrypted with the password.")
+        End Try
+    End Sub
+End Class
