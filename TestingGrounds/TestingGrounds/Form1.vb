acb4b3019fc3f37afa325a6aa59313861548c8a1
Magnus Bakke <magnusbakke@live.com>
2017-03-17 02:57:44 +0100

    Added the FlatForm control
    TODO: Add Questionnaire control

--- TestingGrounds/TestingGrounds/Form1.vb
+++ TestingGrounds/TestingGrounds/Form1.vb
@@ -1,7 +1,47 @@
-﻿Public Class Form1
+﻿Option Strict On
+Option Explicit On
+Option Infer Off
+
+Public Class Form1
     Dim WithEvents Cal As CustomCalendar
+    Dim Test As FlatForm
     Private Sub Setup_Tjener_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Cal = New CustomCalendar(Me, 14, 14, 80, 80, 14, 14)
+        'Cal = New CustomCalendar(Me, 14, 14, 80, 80, 14, 14)
+        Test = New FlatForm(400, 10)
+        With Test
+            .Parent = Me
+            .Height = 1000
+            .AddField(FormElementType.CheckBox, 120)
+            .AddField(FormElementType.Label)
+            .AddField(FormElementType.CheckBox, 120)
+            .AddField(FormElementType.CheckBox, 160)
+            .AddField(FormElementType.CheckBox)
+            .AddField(FormElementType.TextField)
+            .AddField(FormElementType.Radio, 120, True)
+            .AddField(FormElementType.Radio)
+            .AddRadioContext()
+            .AddField(FormElementType.Radio, 120, True)
+            .AddField(FormElementType.Radio)
+        End With
+        ' Legg til ExtrudeBottom, Right og Top
+        Test.Field(1, 2).Extrude(FieldExtrudeSide.Left, 10)
+        With Test.Field(0, 1)
+            '.Extrude(FieldExtrudeSide.Bottom, 10)
+            .Value = "Dette er et eksempel på både vertikal og horisontal sammensmelting."
+        End With
+        With Test.Field(1, 2)
+            '.SwitchHeader(False)
+            .SecondaryValue = "Test"
+        End With
+        Test.MergeWithAbove(1, 1)
+        Test.MergeWithAbove(1, 2, -1)
+        With Test.Field(2, 0)
+            .Value = "Hei"
+            .Header.Text = "Har du noen ekstra kommentarer?"
+        End With
+        Debug.Print(Test.FieldCount & ", " & Test.RowCount)
+        Test.HeightToContent()
+        Test.Display()
     End Sub
     Private Sub CalMouseEnter(Sender As CustomCalendar.CalendarDay) Handles Cal.MouseEnter
         Select Case Sender.Area
@@ -23,8 +63,18 @@
         End Select
         Dim Shad As New BoxShadow(Sender, Color.FromArgb(0, 50, 60))
     End Sub
-
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Cal.Dispose()
+        'Cal.Dispose()
+        Dim Result()() As HeaderValuePair = Test.Result
+        MsgBox(Result(2)(0).Value.ToString)
+        Test.Dispose()
+    End Sub
+    Private Sub Me_Resize() Handles MyBase.Resize
+        If Test IsNot Nothing Then
+            With Test
+                .Left = CInt((Me.ClientRectangle.Width / 2) - (Test.Width / 2))
+                .Top = CInt((Me.ClientRectangle.Height / 2) - (Test.Height / 2))
+            End With
+        End If
     End Sub
 End Class
